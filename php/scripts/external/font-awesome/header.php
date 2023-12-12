0c81a526766b96c6b0b5393d69b8d04ae1c44196
Scott Shealy <vsshealy@gmail.com>
2023-12-12 13:48:56 -0500

    Added php files

---
+++ php/scripts/external/font-awesome/header.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * php/scripts/external/font-awesome/header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2023.12.12)
+     * @copyright 2023 (2023.12.12)
+    **/
+?>
+
+<script src="https://kit.fontawesome.com/cf6277ea30.js" crossorigin="anonymous"></script>
---
+++ php/scripts/external/google-analytics/header.php
@@ -0,0 +1,9 @@
+<?php 
+    /**
+     * php/scripts/external/google-analytics/header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2023.12.12)
+     * @copyright 2023 (2023.12.12)
+    **/
+?>
